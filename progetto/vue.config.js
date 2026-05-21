171c8c07cd727ec17b55d10c42155e259495cb52
michelerocc <rocchini.michele.studente@itispaleocapa.it>
2026-05-21 20:26:26 +0000

    com

--- progetto/vue.config.js
+++ progetto/vue.config.js
@@ -1,6 +1,4 @@
-const { defineConfig } = require("@vue/cli-service");
+module.exports = {
+  publicPath: './'
 
-module.exports = defineConfig({
-  transpileDependencies: true,
-  publicPath: "./"
-});
+};
